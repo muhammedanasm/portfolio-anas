4ab1430f2d2d9c771bc79b3df396bd136c8275bf
muhammedanasm <anasmanasm459@gmail.com>
2026-03-27 01:29:57 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://muhammed-anas.vercel.app"; // meetanas.com
+  const baseUrl = "https://getanas.com/";
 
   return [
     {
